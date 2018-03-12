1f9be1de8cacc433bcbb504b46ba7824fde391c6
winstonaws <31297115+winstonaws@users.noreply.github.com>
2018-03-12 13:33:23 -0700

    Fix dockerfile entrypoints to use the correct python version (#6)
    * Fix dockerfile entrypoints to use the correct python version
    
    * Fix test comments

--- test/integ/test_py_version.py
+++ test/integ/test_py_version.py
@@ -1,20 +1,54 @@
+from __future__ import print_function
+
+from sagemaker import fw_utils
+from sagemaker.utils import sagemaker_timestamp
 import docker_utils
+import utils
+import os
+import json
 
 
-# The image should have an appropriate version of python installed.
-def test_py2(py_version, docker_image):
+# The image should run the user-provided code using the right python version when training.
+def test_train_py_version(docker_image, sagemaker_session, py_version, opt_ml):
+    resource_path = 'test/resources/py_version/code'
 
+    s3_source_archive = fw_utils.tar_and_upload_dir(session=sagemaker_session.boto_session,
+                                bucket=sagemaker_session.default_bucket(),
+                                s3_key_prefix=sagemaker_timestamp(),
+                                script='usermodule.py',
+                                directory=resource_path)
+
+    hp = _py_version_dict(py_version)
+
+    utils.create_config_files('usermodule.py', s3_source_archive.s3_prefix, opt_ml, additional_hp=hp)
+    os.makedirs(os.path.join(opt_ml, 'model'))
+    docker_utils.train(docker_image, opt_ml)
+
+    # The usermodule.py train_fn will assert on the expected python versions passed in through hyperparameters,
+    # and training will fail if they are incorrect.
+    success_file = 'output/success'
+    assert os.path.exists(os.path.join(opt_ml, success_file)), 'expected file not found: {}'.format(success_file)
+
+
+# The image should run the user-provided code using the right python version when hosting.
+def test_hosting_py_version(docker_image, py_version, opt_ml):
+    resource_path = 'test/resources/py_version'
+    utils.copy_resource(resource_path, opt_ml, 'code')
+
+    input = json.dumps(_py_version_dict(py_version))
+
+    with docker_utils.HostingContainer(image=docker_image,
+                                       opt_ml=opt_ml, script_name='usermodule.py') as c:
+        c.ping()
+        # We send the json of the expect py versions in the request. The usermodule.py transform_fn will assert on the python versions,
+        # and this request will fail and throw an exception if they are incorrect.
+        c.invoke_endpoint(input)
+
+
+def _py_version_dict(py_version):
     maj_to_minor = {2: 7, # Need Python 2.7 for Python 2
                     3: 4} # Need Python 3.4 or above for Python 3
-    expected_maj = int(py_version)
-    expected_min_minor = maj_to_minor[expected_maj]
 
-    with docker_utils.Container(docker_image) as c:
-        c.execute_command([
-            'python', '-c', '; '.join([
-                'import sys',
-                'maj, min = sys.version_info[0:2]',
-                'print \'running python {}.{}\'.format(maj, min)',
-                'assert maj == {} and min >= {}'.format(expected_maj, expected_min_minor)
-            ])]
-        )
+    return {'py_major_version': str(py_version),
+            'py_minimum_minor_version': str(maj_to_minor[py_version])}
+
---
+++ test/resources/py_version/code/usermodule.py
@@ -0,0 +1,32 @@
+import sys
+import json
+
+
+def train(hyperparameters, num_cpus, num_gpus, channel_input_dirs, **kwargs):
+    _assert_py_version(hyperparameters)
+
+
+class DummyModel(object):
+      def predict(self, data):
+          return data
+  
+  
+def model_fn(model_dir):
+  return DummyModel()
+
+
+def transform_fn(model, data, input_content_type, output_content_type):
+    print('input object: {}'.format(data))
+    _assert_py_version(json.loads(data))
+    return data, "application/json"
+
+
+def _assert_py_version(version_dict):
+    print('python version info: {}'.format(sys.version_info))
+    major_v, minor_v = sys.version_info[0:2]
+    expected_major_v = int(version_dict['py_major_version'])
+    minimum_minor_v = int(version_dict['py_minimum_minor_version'])
+     
+    assert major_v == expected_major_v, 'python major version must be {}'.format(expected_major_v)
+    assert minor_v >= minimum_minor_v, 'python minor version must be >= {}'.format(minimum_minor_v)
+
