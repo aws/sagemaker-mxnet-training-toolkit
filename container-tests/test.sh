efc4e4a8d0698ff864ed3ef1535fb24bab875812
Winston Dong <win@amazon.com>
2018-03-29 19:13:02 +0000

    Add headers for Apache license

--- container-tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$CONTBUILD_PACKAGE_NAME" =~ GpuImage$ ]]
-then
-	echo "Skipping container tests for GPU image."
-else
-	pip install pytest
-	pytest /opt/amazon/container-tests/*.py	
-fi
