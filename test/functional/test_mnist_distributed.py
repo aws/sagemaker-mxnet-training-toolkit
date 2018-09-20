901b3f873977ae7c053434cd56a25fc5af54b6d9
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2018-09-20 15:57:14 -0700

    skip sagemaker integ test

--- test/functional/test_mnist_distributed.py
+++ test/functional/test_mnist_distributed.py
@@ -1,22 +1,24 @@
 #  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License").
 #  You may not use this file except in compliance with the License.
 #  A copy of the License is located at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
-#  or in the "license" file accompanying this file. This file is distributed 
-#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-#  express or implied. See the License for the specific language governing 
+#
+#  or in the "license" file accompanying this file. This file is distributed
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from sagemaker import Session
+import os
+
+import numpy as np
+import pytest
 from sagemaker.mxnet.estimator import MXNet
 from sagemaker.utils import sagemaker_timestamp
+
 from timeout import timeout, timeout_and_delete_endpoint
-import numpy as np
-import os
 
 
 class MXNetTestEstimator(MXNet):
@@ -33,16 +35,16 @@ class MXNetTestEstimator(MXNet):
         return model
 
 
+@pytest.mark.skip
 def test_mxnet_distributed(sagemaker_session, ecr_image, instance_type):
     with timeout(minutes=15):
         script_path = 'test/resources/mnist/mnist.py'
         data_path = 'test/resources/mnist'
 
         mx = MXNetTestEstimator(entry_point=script_path, role='SageMakerRole',
-                   train_instance_count=2, train_instance_type=instance_type,
-                   sagemaker_session=sagemaker_session,
-                   docker_image_uri=ecr_image)
-
+                                train_instance_count=2, train_instance_type=instance_type,
+                                sagemaker_session=sagemaker_session,
+                                docker_image_uri=ecr_image)
 
         prefix = 'mxnet_mnist/{}'.format(sagemaker_timestamp())
         train_input = mx.sagemaker_session.upload_data(path=os.path.join(data_path, 'train'),
@@ -50,9 +52,9 @@ def test_mxnet_distributed(sagemaker_session, ecr_image, instance_type):
         test_input = mx.sagemaker_session.upload_data(path=os.path.join(data_path, 'test'),
                                                       key_prefix=prefix + '/test')
         mx.fit({'train': train_input, 'test': test_input})
-        
+
     with timeout_and_delete_endpoint(estimator=mx, minutes=30):
         predictor = mx.deploy(initial_instance_count=1, instance_type=instance_type)
 
-        data=np.zeros(shape=(1, 1, 28, 28))
+        data = np.zeros(shape=(1, 1, 28, 28))
         predictor.predict(data)
