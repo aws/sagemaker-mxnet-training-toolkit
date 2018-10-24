d13fed013d6a003fa338b439652b07494d1ea787
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2018-10-24 10:45:58 -0700

    Fix serving, update integ tests, and allow user-supplied transform_fn (#40)

--- test/integ/test_hosting.py
+++ test/integ/test_hosting.py
@@ -13,24 +13,27 @@
 from __future__ import absolute_import
 
 import json
+import os
 
-import pytest
+from sagemaker.mxnet.model import MXNetModel
 
-import docker_utils
-import utils
+import local_mode
 
 
 # The image should use the model_fn and transform_fn defined
 # in the user-provided script when serving.
-@pytest.mark.skip
-def test_hosting(docker_image, opt_ml, processor):
-    resource_path = 'test/resources/dummy_hosting'
-    utils.copy_resource(resource_path, opt_ml, 'code')
+def test_hosting(docker_image, sagemaker_local_session):
+    resource_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'dummy_hosting')
+    m = MXNetModel(os.path.join('file://', resource_path, 'code'), 'SageMakerRole',
+                   os.path.join(resource_path, 'code', 'dummy_hosting_module.py'),
+                   image=docker_image, sagemaker_session=sagemaker_local_session)
 
     input = json.dumps({'some': 'json'})
 
-    with docker_utils.HostingContainer(image=docker_image, processor=processor,
-                                       opt_ml=opt_ml, script_name='dummy_hosting_module.py') as c:
-        c.ping()
-        output = c.invoke_endpoint(input)
-        assert input == output
+    with local_mode.lock():
+        try:
+            predictor = m.deploy(1, 'local')
+            output = predictor.predict(input)
+            assert input == output
+        finally:
+            sagemaker_local_session.delete_endpoint(m.endpoint_name)
