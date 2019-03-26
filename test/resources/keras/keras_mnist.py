64c5c8ed68e34fae50b6ac9521a0a28156fa8cff
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-03-26 11:32:24 -0700

    Set 'channels first' for Keras (#74)

--- test/resources/keras/keras_mnist.py
+++ test/resources/keras/keras_mnist.py
@@ -43,9 +43,8 @@ def main(batch_size, epochs, num_classes, training_channel, model_dir):
         x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
         input_shape = (1, img_rows, img_cols)
     else:
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
+        raise ValueError('Unexpected image data format (expected "channels_first"): {}'
+                         .format(K.image_data_format()))
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
@@ -90,7 +89,7 @@ def main(batch_size, epochs, num_classes, training_channel, model_dir):
     data_name, data_shapes = keras.models.save_mxnet_model(model=model, prefix=model_prefix,
                                                            epoch=0)
 
-    signature = [{'name': data_name, 'shape': [dim for dim in data_desc.shape]}
+    signature = [{'name': data_name[0], 'shape': [dim for dim in data_desc.shape]}
                  for data_desc in data_shapes]
     with open(os.path.join(model_dir, 'model-shapes.json'), 'w') as f:
         json.dump(signature, f)
