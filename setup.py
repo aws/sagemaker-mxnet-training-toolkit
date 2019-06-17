22b037fa30d90021b3c6a0faa16eac99bafdc825
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-06-17 09:42:20 -0700

    change: explicitly set lower bound for botocore version
    The recently-adding hyperparameter tuning test (#95) exposed
    some issue with our dependency chain not having an
    updated version of botocore.
    
    Similar: https://github.com/aws/sagemaker-tensorflow-container/pull/187

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     install_requires=['sagemaker-containers>=2.2.5', 'retrying==1.3.3'],
     extras_require={
         'test': ['tox', 'pluggy==0.11', 'flake8', 'pytest==4.5.0', 'pytest-cov', 'pytest-xdist',
-                 'mock', 'sagemaker', 'docker-compose']
+                 'mock', 'sagemaker', 'docker-compose', 'botocore>=1.12.140']
     },
 )
