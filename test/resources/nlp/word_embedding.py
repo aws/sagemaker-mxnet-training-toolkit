d036a8c95ac1cc65efbf76c186fe8f0be168122c
Leonard Lausen <lausen@amazon.com>
2020-01-23 04:06:49 +0000

    Fix licensing

--- test/resources/nlp/word_embedding.py
+++ test/resources/nlp/word_embedding.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 # Example taken from
 # https://gluon-nlp.mxnet.io/examples/word_embedding/word_embedding.html
-# Licensed under Apache License Version 2.0, January 2004
 
 # # Pre-trained Word Embeddings
 # 
