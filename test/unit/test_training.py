55d93a48d2f81cc8a6c041ee5938060328c1ac4d
Ajay Karpur <akarpur@amazon.com>
2020-02-03 11:58:18 -0800

    Revert "fix: remove call to deprecated function download_and_install"
    This reverts commit 66236f41d02f4997bc9eb2fae4ccd2bec206c3da.

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -71,8 +71,9 @@ def distributed_training_env():
 @patch('subprocess.Popen')
 @patch('sagemaker_mxnet_container.training._host_lookup')
 @patch('sagemaker_mxnet_container.training._verify_hosts')
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
 @patch('sagemaker_containers.beta.framework.entry_point.run')
-def test_train_for_distributed_scheduler(run_entry_point, verify_hosts,
+def test_train_for_distributed_scheduler(run_entry_point, download_and_install, verify_hosts,
                                          host_lookup, popen, distributed_training_env):
     host_lookup.return_value = IP_ADDRESS
 
@@ -92,6 +93,7 @@ def test_train_for_distributed_scheduler(run_entry_point, verify_hosts,
 
     popen.assert_has_calls(calls)
 
+    download_and_install.assert_called_with(MODULE_DIR)
     run_entry_point.assert_called_with(MODULE_DIR,
                                        MODULE_NAME,
                                        distributed_training_env.to_cmd_args(),
@@ -103,8 +105,9 @@ def test_train_for_distributed_scheduler(run_entry_point, verify_hosts,
 @patch('subprocess.Popen')
 @patch('sagemaker_mxnet_container.training._host_lookup')
 @patch('sagemaker_mxnet_container.training._verify_hosts')
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
 @patch('sagemaker_containers.beta.framework.entry_point.run')
-def test_train_for_distributed_worker(run_entry_point, verify_hosts,
+def test_train_for_distributed_worker(run_entry_point, download_and_install, verify_hosts,
                                       host_lookup, popen, distributed_training_env):
     host_lookup.return_value = IP_ADDRESS
 
@@ -118,6 +121,7 @@ def test_train_for_distributed_worker(run_entry_point, verify_hosts,
 
     popen.assert_called_once_with(MXNET_COMMAND, shell=True, env=server_env)
 
+    download_and_install.assert_called_with(MODULE_DIR)
     run_entry_point.assert_called_with(MODULE_DIR,
                                        MODULE_NAME,
                                        distributed_training_env.to_cmd_args(),
@@ -125,10 +129,12 @@ def test_train_for_distributed_worker(run_entry_point, verify_hosts,
                                        runner=framework.runner.ProcessRunnerType)
 
 
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
 @patch('sagemaker_containers.beta.framework.entry_point.run')
-def test_train_for_single_machine(run_entry_point,
+def test_train_for_single_machine(run_entry_point, download_and_install,
                                   single_machine_training_env):
     training.train(single_machine_training_env)
+    download_and_install.assert_called_with(MODULE_DIR)
     run_entry_point.assert_called_with(MODULE_DIR,
                                        MODULE_NAME,
                                        single_machine_training_env.to_cmd_args(),
