7077972827c26118bb5f3d638cabb0cf743bfae8
Dana Benson <danabens@amazon.com>
2020-03-11 13:46:39 -0700

    install sm experiments always when python 3.6 or greater

--- setup.py
+++ setup.py
@@ -56,12 +56,14 @@ setup(
 
     # We don't declare our dependency on mxnet here because we build with
     # different packages for different variants (e.g. mxnet-mkl and mxnet-cu90).
-    install_requires=['sagemaker-containers>=2.4.10.post0', 'retrying==1.3.3'],
+    install_requires=[
+        'sagemaker-containers>=2.4.10.post0',
+        'retrying==1.3.3',
+        'sagemaker-experiments==0.1.7;python_version>="3.6"'],
     extras_require={
         'test': ['tox', 'pluggy==0.11', 'flake8', 'pytest==4.5.0', 'pytest-cov', 'pytest-xdist',
                  'mock', 'sagemaker==1.28.1', 'docker-compose', 'boto3==1.10.32', 'six==1.13.0',
                  'python-dateutil>=2.1,<2.8.1', 'botocore==1.13.32', 'awscli==1.16.278',
-                 'requests_mock==1.7.0'],
-        ':python_version=="3.6"': ['sagemaker-experiments==0.1.7']
+                 'requests_mock==1.7.0']
     },
 )
