4370c43c5b38099802b407ee8056a4ccd96a04aa
SK <saravsak@amazon.com>
2020-03-19 15:09:32 -0700

    Updated pytest version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-test_dependencies = ['tox', 'pluggy==0.11', 'flake8', 'pytest==4.5.0', 'pytest-cov', 'pytest-xdist',
+test_dependencies = ['tox', 'pluggy==0.11', 'flake8', 'pytest==5.0', 'pytest-cov', 'pytest-xdist',
                      'mock', 'sagemaker==1.28.1', 'docker-compose', 'boto3==1.10.32', 'six==1.13.0',
                      'python-dateutil>=2.1,<2.8.1', 'botocore==1.13.32', 'awscli==1.16.278',
                      'requests_mock==1.7.0']
