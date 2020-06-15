dde8db240a43ee2a14e98df894c48c86e33a7eb8
ChaiBapchya <chai.bapat@gmail.com>
2020-06-15 14:38:22 -0700

    add horovod test

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -66,6 +66,18 @@ def distributed_training_env():
 
     return env
 
+@patch('sagemaker_training.entry_point.run')
+def test_train_horovod(run_module, single_machine_training_env):
+    single_machine_training_env.additional_framework_parameters = {
+        training.LAUNCH_MPI_ENV_NAME: True,
+    }
+
+    training.train(single_machine_training_env)
+    run_module.assert_called_with(uri=MODULE_DIR, 
+				  user_entry_point=MODULE_NAME,
+				  args=single_machine_training_env.to_cmd_args(),
+                                  env_vars=single_machine_training_env.to_env_vars(),
+                                  runner_type=runner.MPIRunnerType)
 
 @patch('os.environ', {})
 @patch('subprocess.Popen')
